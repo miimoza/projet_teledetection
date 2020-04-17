e436b210bb27e6504bdf3c19a84b4862729413df
Dorian Vinai <dorian.vinai@epita.fr>
2020-04-17 15:03:47 +0200

    initial commit

---
+++ NUAGES_SRC/EXE/CMakeFiles/imaProjet.exe.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bug/TELE/NUAGES_SRC/compute.c" "/home/bug/TELE/NUAGES_SRC/EXE/CMakeFiles/imaProjet.exe.dir/compute.c.o"
+  "/home/bug/TELE/NUAGES_SRC/main.c" "/home/bug/TELE/NUAGES_SRC/EXE/CMakeFiles/imaProjet.exe.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gtk-2.0"
+  "/usr/lib/gtk-2.0/include"
+  "/usr/include/pango-1.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/glib-2.0/include"
+  "/usr/include/harfbuzz"
+  "/usr/include/fribidi"
+  "/usr/include/freetype2"
+  "/usr/include/libpng16"
+  "/usr/include/cairo"
+  "/usr/include/pixman-1"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/libmount"
+  "/usr/include/blkid"
+  "/usr/include/atk-1.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ NUAGES_SRC/EXE/CMakeFiles/imaProjet.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imaProjet.exe.dir/compute.c.o"
+  "CMakeFiles/imaProjet.exe.dir/main.c.o"
+  "imaProjet.exe"
+  "imaProjet.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/imaProjet.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
